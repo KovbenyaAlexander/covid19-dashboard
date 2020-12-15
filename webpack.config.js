3a73c57eb38c5441718b22cde9b9d175a90c56b9
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2020-12-15 13:59:09 +0300

    refactor: add files

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+	const isProduction = options.mode === 'production';
+
+  const config = {
+		mode: isProduction ? 'production' : 'development',
+		devtool: isProduction ? 'eval' : 'source-map',
+		watch: !isProduction,
+    entry: ['./src/index.js', './src/styles/style.scss'],
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+					test: /\.scss$/,
+					use: [
+            'style-loader',
+						'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        }
+      ]
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+				template: './src/index.html',
+			}),
+    ]
+  }
+  return config;
+}
